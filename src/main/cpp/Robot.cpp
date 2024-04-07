786b5daa2eb132bf1bd4e68223400b7c0aafd69d
MrMatt3 <Linmatthew2007@gmail.com>
2024-04-07 10:44:09 -0700

    Added function call for delayed auto

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -266,7 +266,7 @@ void Robot::AutonomousInit() {
   } else if (m_autoSelected == kAutoCustomSpeaker) {
     autoSpeaker();
   } else if (m_autoSelected == kAutoCustomDelayedSpeaker) {
-
+    autoDelayedSpeaker();
   } else {
     autoLeave();
   }
